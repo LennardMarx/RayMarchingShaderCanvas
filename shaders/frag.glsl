7917a04884a5578fa2aa0f7649038d5c31c758c2
LennardMarx <l.marx@gmx.de>
2024-01-10 23:03:05 +0100

    blank canvas again

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -96,71 +96,8 @@ float sdCutHollowSphere( vec3 p, float r, float h, float t )
 //   return smin(ground, smin(sphere, box, 10.0f), 5.0f);
 // }
 
-float rotatingSpineQuarter(vec3 p, vec2 pos, float oX, float oZ, float timing){
-
-  vec3 pBox = p;
-
-  pBox.xz += pos;
-  
-  pBox.x += oX;
-  pBox.z += oZ;
-  
-  pBox.xz = mod(pBox.xz, 15.0f) - 7.5f;
-  
-  pBox.y += 1.5f + (3.0f * sin(1.5f*gTime + timing) - 0.0f);
-  pBox.x -= oX;
-  pBox.z -= oZ;
-  pBox.xz *= rot2D(1.5f*pBox.y*sin(1.5f*gTime + timing));
-  pBox.x += oX; //*sin(2.0f*gTime);
-  pBox.z += oZ; //*sin(2.0f*gTime);
-  pBox.x += 0.1f*(pBox.y)*sign(-oX);
-  pBox.z += 0.1f*(pBox.y)*sign(-oZ);
-  
-  float box = sdBox(pBox, vec3(0.1f, 3.0f, 0.1f));
-  return box;
-}
-
-float rotatingSpine(vec3 p, vec2 pos, float offset, float timing){
-  float box1 = rotatingSpineQuarter(p, pos, -offset, -offset, timing);
-  float box2 = rotatingSpineQuarter(p, pos,  offset, -offset, timing);
-  float box3 = rotatingSpineQuarter(p, pos, -offset,  offset, timing);
-  float box4 = rotatingSpineQuarter(p, pos,  offset,  offset, timing);
-
-  return min(box1, min(box2, min(box3, box4)));
-}
-
-float rand(vec2 co){
-  return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-}
- 
-float randomSpines(vec3 p, int amount){
-  // float dist = rotatingSpine(p, vec2(rand(vec2(float(amount), float(amount)))), rand(vec2(float(amount), float(amount))), 0.3f, rand(vec2(float(amount), float(amount))));
-  // float dist = rotatingSpine(p, vec2(rand(gCamPos.xz), rand(gCamPos.xz)), 0.3f, rand(gCamPos.xz));
-  
-  float i_float = float(amount);
-  vec2 r = vec2(i_float, i_float);
-  // vec2 r = vec2(1.0f, 1.0f);
-  float dist = rotatingSpine(p, vec2(rand(r), rand(r)), 0.3f, rand(r));
-  
-  for (int i = 0; i < amount-1; i++){
-    i_float = float(i);
-    float r = rand(vec2(i_float+9.213f, i_float+3.546f))*50.0f;
-    float r2 = rand(vec2(i_float+3.387f, i_float+7.189f))*50.0f;
-    float r3 = rand(vec2(i_float+3.907f, i_float+7.028f))*50.0f;
-    float dist2 = rotatingSpine(p, vec2(r, r2), 0.3f, r3);
-    dist = min(dist, dist2);
-  }
-  return dist;
-}
-
 float map(vec3 p){
   
-  // float box1 = rotatingSpine(p, vec2(0.0f, 0.0f), 0.3f, 0.0f);
-  // float box2 = rotatingSpine(p, vec2(2.0f, 0.0f), 0.3f, 2.0f);
-  // float box3 = rotatingSpine(p, vec2(3.0f, -1.0f), 0.3f, 3.9f);
-
-  // box1 = min(box1, min(box2, box3));
-  // float box1 = randomSpines(p, 10);
   vec3 q = p;
   q.x += sin(gTime);
   float box1 = sdBox(q, vec3(1.0f));
@@ -191,11 +128,6 @@ void main()
   for(i = 0; i < 80; i++){
     vec3 p = ro + rd * t;           // position along the ray 
     
-    // p.xy *= rot2D(0.1f*gTime + 0.1f*sin(gTime) + t*0.2f + t*0.05f*sin(0.5f*gTime)); // Const Rotation, changing rotation speed, constant twist, changing twist amount
-    // p.xy *= rot2D(t*0.2f*sin(time + 10.0f) + 0.1f*sin(time));
-
-    // p.y += sin(t)*0.35f;
-    
     float d = map(p);               // current distance to the scene
 
     t += d;                         // march the ray
@@ -204,7 +136,8 @@ void main()
     if(d < 0.001f || t > 100.0f) break;
   }
 
-  col = vec3(t * 0.02f + float(i)*0.002f, t*0.05f + float(i)*0.005f, t*0.07f + float(i)*0.007f);
+  // col = vec3(t * 0.02f + float(i)*0.002f, t*0.05f + float(i)*0.005f, t*0.07f + float(i)*0.007f);
+  col = vec3(t* 0.04f + float(i)*0.004f);
   // col = palette(t * 0.04f + float(i)*0.005f);
   
   color = vec4(col, 1.0f);
