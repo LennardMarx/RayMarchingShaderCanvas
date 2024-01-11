df50420eb862fdcfe855f52ab9a8b3a0399b2b1a
LennardMarx <l.marx@gmx.de>
2024-01-11 22:46:35 +0100

    hilly valley with perlin noise

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -19,6 +19,82 @@ in vec3 v_vertexPositions;
 
 out vec4 color;
 
+//
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+// 
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+10.0)*x);
+}
+
+float snoise(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+
+
+
+
 float sdBox( vec3 p, vec3 b )
 {
   vec3 q = abs(p) - b;
@@ -102,14 +178,14 @@ float rotatingSpineQuarter(vec3 p, vec2 pos, float oX, float oZ, float timing){
 
   pBox.xz += pos;
   
-  pBox.x += oX;
-  pBox.z += oZ;
+  // pBox.x += oX;
+  // pBox.z += oZ;
   
   pBox.xz = mod(pBox.xz, 15.0f) - 7.5f;
   
   pBox.y += 1.5f + (3.0f * sin(1.5f*gTime + timing) - 0.0f);
-  pBox.x -= oX;
-  pBox.z -= oZ;
+  // pBox.x -= oX;
+  // pBox.z -= oZ;
   pBox.xz *= rot2D(1.5f*pBox.y*sin(1.5f*gTime + timing));
   pBox.x += oX; //*sin(2.0f*gTime);
   pBox.z += oZ; //*sin(2.0f*gTime);
@@ -154,6 +230,7 @@ float randomSpines(vec3 p, int amount){
 }
 
 float map(vec3 p){
+  // p.z += gTime*2.4f;
   
   // float box1 = rotatingSpine(p, vec2(0.0f, 0.0f), 0.3f, 0.0f);
   // float box2 = rotatingSpine(p, vec2(2.0f, 0.0f), 0.3f, 2.0f);
@@ -188,8 +265,20 @@ void main()
   for(i = 0; i < 80; i++){
     vec3 p = ro + rd * t;           // position along the ray 
     
-    // p.xy *= rot2D(0.1f*gTime + 0.1f*sin(gTime) + t*0.2f + t*0.05f*sin(0.5f*gTime)); // Const Rotation, changing rotation speed, constant twist, changing twist amount
-    // p.xy *= rot2D(t*0.2f*sin(time + 10.0f) + 0.1f*sin(time));
+    // p.xy *= rot2D(0.01f*gTime + 0.001f*sin(gTime) + t*0.02f + t*0.005f*sin(0.5f*gTime)); // Const Rotation, changing rotation speed, constant twist, changing twist amount
+    // p.xy *= rot2D(t*0.02f*sin(gTime + 10.0f) + 0.01f*sin(gTime));
+    // p.xz *= rot2D(t*0.01f*cos(gTime));
+
+    // p.xy *= rot2D(t*0.005f*cos(gTime + 10.0f));
+    // p.zy *= rot2D(t*0.005f*sin(gTime));
+    p.y -= p.x*p.x * 0.02f;
+    // p.y += p.z*p.z * 0.02f;
+
+    p.y -= 2.0f*snoise(0.05f*p.xz);
+
+
+    
+    // p.y = abs(p.y); // some kind of mirror
 
     // p.y += sin(t)*0.35f;
     
